06b30d89613e349c3af8ed502c9be8f4e1efd13a
정경훈 <ruderud0055@gmail.com>
2024-01-05 16:20:12 +0900

    폼 생성

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,6 +9,16 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    console.warn('Your order has been submitted', this.items, this.checkoutForm.value);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
