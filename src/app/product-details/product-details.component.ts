6f4dfee4a65267cdba7e34124a83c6318eee95fa
정경훈 <ruderud0055@gmail.com>
2024-01-05 14:55:05 +0900

    배송컴포넌트까지

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product, products } from '../products';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
   product?: Product;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private cartService: CartService) {}
 
   // render 시작시점에 실행되는 함수
   ngOnInit() {
@@ -21,5 +22,10 @@ export class ProductDetailsComponent {
 
     // Find the product that correspond with the id provided in route.
     this.product = products.find((product) => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
